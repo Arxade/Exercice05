2f4958ae680f0486709518e8ed77032f5dfb8eea
Alexandre <Alexandre@10.40.6.46>
2020-01-14 13:31:05 +0100

    Ajout de methodes

--- ExerciceE05/src/dao/ProduitDAO.java
+++ ExerciceE05/src/dao/ProduitDAO.java
@@ -1,5 +1,7 @@
 package dao;
 
+import classes.Produit;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,4 +14,18 @@ package dao;
  */
 public class ProduitDAO {
     
+    public void insert(Produit produit)
+    {
+        String requete = "INSERT INTO PRODUITS VALUES (?,?,?,?)";
+    }
+    
+    public void delete(Produit produit)
+    {
+        String requete = "DELETE FROM PRODUITS WHERE NOMPRODUIT = " + produit.getNom();
+    }
+    
+    public void updateNom(Produit produit)
+    {
+        String requete = "UPDATE PRODUITS SET NOMPRODUIT = ? WHERE NOMPRODUIT = " + produit.getNom();
+    }
 }
