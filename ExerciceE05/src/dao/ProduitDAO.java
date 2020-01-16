d763b68628eed8602abbb1aa82d4d3de35c5f5df
Alexandre <Alexandre@10.40.6.46>
2020-01-16 12:01:24 +0100

    ajouts mineur update prixHT Nom

--- ExerciceE05/src/dao/ProduitDAO.java
+++ ExerciceE05/src/dao/ProduitDAO.java
@@ -86,11 +86,23 @@ public class ProduitDAO extends Connexion {
     }
 
     public void updateNom(String ancienNom, String nouveauNom) {
-        String requete = "UPDATE PRODUITS SET NOMPRODUIT = ? WHERE NOMPRODUIT = " + ancienNom;
+        try {
+            statement = connection.createStatement();
+            String requete = "UPDATE PRODUITS SET NOMPRODUIT = " + nouveauNom + " WHERE NOMPRODUIT = " + ancienNom;
+            statement.executeQuery(requete);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProduitDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public void updatePrixHT(String nomProduit, BigDecimal nouveauPrix) {
-        String requete = "UPDATE PRODUITS SET PRIXHTPRODUIT = ? WHERE NOMPRODUIT = " +nomProduit;
+        try {
+            statement = connection.createStatement();
+            String requete = "UPDATE PRODUITS SET PRIXHTPRODUIT = " + nouveauPrix + " WHERE NOMPRODUIT = " + nomProduit;
+            statement.executeQuery(requete);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProduitDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public void updateStock(String nomProduit, int nouveauStock) {
